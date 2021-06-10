1d6f624b83d3e55340acd87ecb5b02c85d632b95
MonkeyEric <1649107451@qq.com>
2021-06-10 21:50:57 +0800

    company

--- utils/ch_login.py
+++ utils/ch_login.py
@@ -1,2 +1,21 @@
 #coding:utf-8
 # 登录验证
+from flask import url_for,redirect,session
+from functools import wraps
+
+def is_login(func):
+    """
+    定义登录注册验证的装饰器
+    :return:check_login
+    """
+    @wraps(func)
+    def check_login(*args, **kwargs):
+        user_id = session.gey("user_id")
+        if user_id:
+            return func(*args, **kwargs)
+        else:
+            return redirect(url_for('user.login'))
+
+    return check_login
+
+
--- webapp/models.py
+++ webapp/models.py
@@ -1,2 +1,15 @@
 #coding:utf-8
 # 存放项目的模型
+from datetime import datetime
+
+# 导入SQLAlchemy模块
+from flask_sqlalchemy import SQLAlchemy
+# 初始化db
+db = SQLAlchemy()
+
+class User(db.Model):
+    """用户模型"""
+    u_id = db.Column(db.Integer, autoincrement=True，primary_key=True)
+    username = db.Column(db.String(16),unique=True)
+    password = db.Column(db.String(250))
+    
