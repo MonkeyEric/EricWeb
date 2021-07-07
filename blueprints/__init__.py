ec35f00e8ec843c15e6c7a87cd08b704c5025953
MonkeyEric <1649107451@qq.com>
2021-07-07 15:41:24 +0800

    commit deleted files

--- blueprints/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-from flask import Blueprint
-
-user_blueprint = Blueprint(
-        'admin_blueprint',
-        __name__,
-        url_prefix= '/api'
-        )
-from . import users, auth
