69b955ccfce88fa94fe210cd5791a6dd7b4f63e1
赵建强 <jianqiang.zhao@lxjcf.com>
2021-07-08 10:50:51 +0800

    edit web from company

--- bluelog/blueprints/admin.py
+++ bluelog/blueprints/admin.py
@@ -1,43 +1,44 @@
 # coding:utf-8
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template
 
-admin_bp = Blueprint('admin',__name__,template_folder='templates')
+admin_bp = Blueprint('admin', __name__, template_folder='templates')
 
-@admin_bp.route('/',methods=['GET'])
+
+@admin_bp.route('/', methods=['GET'])
 def index():
-        return render_template('index.html')
+    return render_template('index.html')
 
 
-@admin_bp.route('/chart',methods=['GET'])
+@admin_bp.route('/chart', methods=['GET'])
 def chart():
-        return render_template('graph_chartjs.html')
+    return render_template('graph_chartjs.html')
 
 
-@admin_bp.route('/data',methods=['GET'])
+@admin_bp.route('/data', methods=['GET'])
 def table_data():
-        return render_template('table_data_tables.html')
+    return render_template('table_data_tables.html')
 
 
-@admin_bp.route('/upload',methods=['GET'])
+@admin_bp.route('/upload', methods=['GET'])
 def upload():
-        return render_template('form_file_upload.html')
+    return render_template('form_file_upload.html')
 
 
-@admin_bp.route('/calendar',methods=['GET'])
+@admin_bp.route('/calendar', methods=['GET'])
 def calendar():
-        return render_template('calendar.html')
+    return render_template('calendar.html')
 
 
-@admin_bp.route('/timeline',methods=['GET'])
+@admin_bp.route('/timeline', methods=['GET'])
 def timeline():
-        return render_template('timeline.html')
+    return render_template('timeline.html')
 
 
-@admin_bp.route('/gallery',methods=['GET'])
+@admin_bp.route('/gallery', methods=['GET'])
 def gallery():
-        return render_template('basic_gallery.html')
+    return render_template('basic_gallery.html')
 
 
-@admin_bp.route('/video',methods=['GET'])
+@admin_bp.route('/video', methods=['GET'])
 def video():
-        return render_template('video.html')
+    return render_template('video.html')
--- bluelog/blueprints/auth.py
+++ bluelog/blueprints/auth.py
@@ -1,14 +1,16 @@
 # coding:utf-8
-from flask import Blueprint, render_template,flash
+from flask import Blueprint, render_template, flash
 from flask_login import current_user
-#from blog.forms import LoginForm
-#from bluelog.models import Admin
-#from bluelog.utils import redirect_back
 
-auth_bp = Blueprint('auth',__name__,template_folder='templates')
+# from blog.forms import LoginForm
+# from bluelog.models import Admin
+# from bluelog.utils import redirect_back
 
-#@auth_bp.route('/login',methods=['GET','POST'])
-#def login():
+auth_bp = Blueprint('auth', __name__, template_folder='templates')
+
+
+# @auth_bp.route('/login',methods=['GET','POST'])
+# def login():
 #    if current_user.is_authenticated:
 #        return redirect(url_for('blog.index'))
 #    form = LoginForm()
@@ -17,17 +19,16 @@ auth_bp = Blueprint('auth',__name__,template_folder='templates')
 #        password = form.password.data
 #        remember = form.remember.data
 
- #       admin = Admin.query.first()
+#       admin = Admin.query.first()
 #        if admin:
 #            if username == admin.username and admin.validate_password(password):
 #                login_user(admin, remember)
 #                flask('Welcome back,','Eric')
 #                return redirect_back()
 #
- #   return render_template('auth/login.html')
+#   return render_template('auth/login.html')
 
 
 @auth_bp.route('/index')
 def index():
     return render_template('index.html')
-
