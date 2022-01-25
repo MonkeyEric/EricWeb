c29101debafc153a815397388bf6cfc44281a213
赵建强 <jianqiang.zhao@lxjcf.com>
2022-01-25 12:07:41 +0800

    v2.1需求添加

--- bluelog/utils/consumer.py
+++
@@ -1,5 +0,0 @@
-# coding:utf-8
-from utils import rm
-
-category = "大神博客"
-rm.consumer(func=main_fav_webs, category=category)
--- bluelog/utils/product.py
+++
@@ -1,5 +0,0 @@
-# coding:utf-8
-from utils import rm
-
-rm.producer(
-    {"url_list": ["http://www.bewindoweb.com/index.php", "https://www.weishidong.com/", "https://cuiqingcai.com/"]})
